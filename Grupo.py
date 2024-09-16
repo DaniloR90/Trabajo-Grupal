cb84340b71dab486782d0c2d35eabc85df340fff
DaniloR90 <danilo.h.rivera.ruiz@gmail.com>
2024-09-16 00:34:10 -0300

    Danilo Enunciado

--- Grupo.py
+++
@@ -1,8 +0,0 @@
-nombre=input("Ingrese su nombre: ").upper()
-sexo=input("Ingrese (F) para femenino y (M) para masculino:").upper()
-if sexo=="F" and nombre[0]<"M":
-    print("Grupo A")
-elif sexo=="M" and nombre[0]>"N":
-    print("Grupo A")
-else:
-    print("Grupo B")
